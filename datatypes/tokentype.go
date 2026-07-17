66da6d008705dce04dfca3cb3346d5ea7c8cfbf0
Yoonyoung Lee <yylee06dk@gmail.com>
2026-07-17 09:13:57 +0000

    Added more scannable tokens, and actual scanning

--- datatypes/tokentype.go
+++ datatypes/tokentype.go
@@ -7,11 +7,35 @@ import (
 type Tokentype int
 
 const (
+
+	// Stage 1
 	Number Tokentype = iota
 	Add
 	Sub
 	Mult
 	Div
+
+	// Stage 1.1
+
+	String
+	Identifier
+	True
+	False
+
+	AddAdd // String concat
+	Bang
+	BangEqual
+	Equal
+	EqualEqual
+	Greater
+	GreaterEqual
+	Less
+	LessEqual
+	And
+	Or
+
+	Error
+	
 	Group
 	EOF
 )
@@ -32,6 +56,36 @@ func (t Tokentype) String() string {
 		return "group"
 	case EOF:
 		return "EOF"
+	case String:
+		return "string"
+	case Identifier:
+		return "identifier"
+	case True:
+		return "true"
+	case False:
+		return "False"
+	case AddAdd:
+		return "++"
+	case Bang:
+		return "!"
+	case BangEqual:
+		return "!="
+	case Equal:
+		return "="
+	case Greater:
+		return ">"
+	case GreaterEqual:
+		return ">="
+	case Less:
+		return "<"
+	case LessEqual:
+		return "<="
+	case And:
+		return "&&"
+	case Or:
+		return "||"
+	case Error:
+		return "Lexical Error"
 	default:
 		return fmt.Sprintf("UnknownTokentype(%d)", t)
 	}
--- scanner/scanner.go
+++ scanner/scanner.go
@@ -2,12 +2,14 @@ package scanner
 
 import (
 	"fmt"
-	"lig/datatypes"
+	dt "lig/datatypes"
 	"strconv"
 )
 
+var dummy dt.Token = dt.Token{dt.Error, 0}
+
 type Scanner struct {
-	Src string
+	Src string 
 	cur int
 }
 
@@ -15,9 +17,9 @@ func New(source string) *Scanner {
 	return &Scanner{source, 0}
 }
 
-func (s *Scanner) ScanTokens() ([]datatypes.Token, error) {
-    var res []datatypes.Token
-    var temp datatypes.Token
+func (s *Scanner) ScanTokens() ([]dt.Token, error) {
+    var res []dt.Token
+    var temp dt.Token
     var err error
 
     for !s.isAtEnd() {
@@ -29,19 +31,20 @@ func (s *Scanner) ScanTokens() ([]datatypes.Token, error) {
     }
 
     // input line terminated without leftover stuffs
-    res = append(res, datatypes.Token{datatypes.EOF, 0})
+    res = append(res, dt.Token{dt.EOF, 0})
 
     return res, nil
 }
 
 type ScanError struct {
-	Source string
-	Pos int
-	Msg string
+	Source string // Bad design? if dealing with source code not repl-like source line.
+	Pos int // Character level position
+	Msg string // Error message
 }
 
 func (e *ScanError) Error() string {
 	return fmt.Sprintf("In position %v in source [%s], error occured: %s\n", e.Pos, e.Source, e.Msg)
+	// This is excessive. Deal such errors at top level.
 }
 
 func (s *Scanner) isAtEnd() bool  {
@@ -49,27 +52,87 @@ func (s *Scanner) isAtEnd() bool  {
 }
 
 
-func (s *Scanner) scanToken() (datatypes.Token, error) {
-	var res datatypes.Token
-	var numErr error
+// When error retval is not used
+func (s *Scanner) scanToken() (dt.Token, error) {
+	var res dt.Token
 
 	c := s.advance()
 
 	switch c {
 		case '+':
-			res = datatypes.Token{datatypes.Add, 0}
+			if s.match('+') {
+				res = dt.Token{dt.AddAdd, 0}
+				break
+			}
+			res = dt.Token{dt.Add, 0}
+
 		case '-':
-			res = datatypes.Token{datatypes.Sub, 0}
+			res = dt.Token{dt.Sub, 0}
+
 		case '*':
-			res = datatypes.Token{datatypes.Mult, 0}
+			res = dt.Token{dt.Mult, 0}
+
 		case '/':
-			res = datatypes.Token{datatypes.Div, 0}
+			res = dt.Token{dt.Div, 0}
+
+		case '!':
+			if s.match('=') {
+				res = dt.Token{dt.BangEqual, 0}
+				break
+			}
+			res = dt.Token {dt.Bang, 0}
+
+		case '>':
+			if s.match('=') {
+				res = dt.Token{dt.GreaterEqual, 0}
+				break
+			}
+			res = dt.Token {dt.Greater, 0}
+
+		case '<':
+			if s.match('=') {
+				res = dt.Token{dt.LessEqual, 0}
+				break
+			}
+			res = dt.Token {dt.Less, 0}
+
+		case '=':
+			if s.match('=') {
+				res = dt.Token {dt.EqualEqual, 0}
+				break
+			}
+			res = dt.Token {dt.Equal, 0}
+
+		case '&':
+			if s.match('&') {
+				res = dt.Token{dt.And, 0}
+				break
+			}
+			return dummy, &ScanError{s.Src, s.cur, fmt.Sprintf("Character %v cannot be used alone", '&')}
+
+		case '|':
+			if s.match('|') {
+				res = dt.Token{dt.Or, 0}
+				break
+			}
+			return dummy, &ScanError{s.Src, s.cur, fmt.Sprintf("Character %v cannot be used alone", '|')}
+
+		case '"':
+			var strErr error
+			res, strErr = s.string()
+			if strErr != nil {
+				return dummy, strErr
+			}
+ 
 		default:
 			if isDigit(c) {
+				var numErr error
 				res, numErr = s.number()
 				if numErr != nil {
 					return res, numErr
 				}
+			} else if isAlpha(c) {
+				res = s.identifier()
 			} else {
 				return res, &ScanError{s.Src, s.cur, "Unexpected character"}
 			}
@@ -89,30 +152,73 @@ func (s *Scanner) isWhitespace() bool {
 	return (s.Src[s.cur] == ' ' || s.Src[s.cur] == '\n' || s.Src[s.cur] == '\t')
 }
 
-func (s *Scanner) number() (datatypes.Token, error) {
+func (s *Scanner) number() (dt.Token, error) {
 	start := s.cur-1
 	for !s.isAtEnd() && isDigit(s.peek()) {
-		s.cur += 1
+		_ = s.advance()
 	}
 
 	num, atoiErr := strconv.Atoi(s.Src[start:s.cur])
 
 	if(atoiErr != nil) {
-		return datatypes.Token{datatypes.Number, 0}, fmt.Errorf("Failed to parse string to int: %w", atoiErr)
+		return dummy, fmt.Errorf("Failed to parse string to int: %w", atoiErr)
 	}
 
-	return datatypes.Token{datatypes.Number, num}, nil
+	return dt.Token{dt.Number, num}, nil
+}
+
+func (s *Scanner) string() (dt.Token, error) {
+	start := s.cur // right after "
+	for !s.isAtEnd() && (s.peek() != '"') {
+		_ = s.advance()
+	}
+
+	stringVal := s.Src[start:s.cur]
+
+	if s.isAtEnd() {
+		return dummy, &ScanError{s.Src, s.cur, fmt.Sprintf("Unterminated string: %s", stringVal)}
+	}
+
+	return dt.Token{dt.String, stringVal}, nil
+}
+
+func (s *Scanner) identifier() dt.Token {
+	start := s.cur-1
+	for !s.isAtEnd() && isAlphaNumeric(s.peek()) {
+		_ = s.advance()
+	}
+
+	name := s.Src[start:s.cur]
+
+	return dt.Token{dt.Identifier, name}
 }
 
 func (s *Scanner) peek() byte {
 	return s.Src[s.cur]
 }
 
+func (s *Scanner) advance() byte {
+	s.cur += 1
+	return s.Src[s.cur-1]
+}
+
+// Consumes byte if matches
+func (s *Scanner) match(expected byte) bool {
+	if s.peek() == expected {
+		_ = s.advance()
+		return true
+	}
+	return false
+}
+
 func isDigit(c byte) bool {
 	return c >= '0' && c <= '9'
 }
 
-func (s *Scanner) advance() byte {
-	s.cur += 1
-	return s.Src[s.cur-1]
+func isAlpha(c byte) bool {
+	return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')
+}
+
+func isAlphaNumeric(c byte) bool {
+	return isDigit(c) || isAlpha(c)
 }
